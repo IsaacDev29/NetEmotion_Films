e5450b51f35639d08db6eddbab5638b260ba11a8
Isaac Daniel <robledogeneric@gmail.com>
2025-01-24 07:42:35 -0600

    Función Obtener Películas Por Id Creada

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ async function obtenerPeliculas(){
         console.error('Error al obtener películas:', error.message);
     }
 }
+
+async function obtenerPeliculaPorID(id) {
+    try {
+      const url = `https://www.freetestapi.com/api/v1/movies/${id}`;
+      const respuesta = await fetch(url);
+      if (!respuesta.ok) {
+        throw new Error(`Error: ${respuesta.status}`);
+      }
+      const datos = await respuesta.json();
+      console.log('Pelicula:', datos);
+    } catch (error) {
+      console.error(`Error al obtener la película con ID ${id}:`, error.message);
+    }
+}
+  
+
+  
