4e129f524ad23b9a8fd9176975dcf3b038e7e567
Isaac Daniel <robledogeneric@gmail.com>
2025-01-24 12:58:30 -0600

    Información Películas De Api Aplicado

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 async function obtenerPeliculas(){
     try {
         const url = 'https://www.freetestapi.com/api/v1/movies';
@@ -25,6 +27,71 @@ async function obtenerPeliculaPorID(id) {
       console.error(`Error al obtener la película con ID ${id}:`, error.message);
     }
 }
-  
+// peliculas de la cartelera1
+async function cargarPeliculasCartelera1() {
+    try {
+      const API_URL = 'https://www.freetestapi.com/api/v1/movies?limit=3';
+      const listaCartelera = document.querySelector('.filaCartelera');
+      const respuesta = await fetch(API_URL);
+      if (!respuesta.ok) throw new Error('Error al obtener las películas');
+      const peliculas = await respuesta.json();
+    
+      listaCartelera.innerHTML = '';
 
+      peliculas.forEach((pelicula) => {
+        const peliculaHTML = `
+          <div class="col-lg-4 columnaTarjeta">
+            <div class="card tarjeta" style="width: 18rem;">
+              <img src="https://fakeimg.pl/300x150" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">${pelicula.title}</h5>
+                <p class="card-text">Director: ${pelicula.director}</p>
+                <p class="card-text">Año: ${pelicula.year}</p>
+                <p class="card-text">Rating: ${pelicula.rating}</p>
+              </div>
+            </div>
+          </div>
+        `;
+        listaCartelera.innerHTML += peliculaHTML;
+      });
+    } catch (error) {
+      console.error('Error al cargar películas:', error.message);
+    }
+}
+// peliculas de la cartelera 2
+async function cargarPeliculasCartelera2() {
+  try {
+    const API_URL = 'https://www.freetestapi.com/api/v1/movies?limit=9';
+    const listaCartelera = document.querySelector('.cartelera2');
+    const respuesta = await fetch(API_URL);
+    if (!respuesta.ok) throw new Error('Error al obtener las películas');
+    const peliculas = await respuesta.json();
+  
+    listaCartelera.innerHTML = '';
+
+    peliculas.forEach((pelicula) => {
+      const peliculaHTML = `
+        <div class="col-lg-4 columnaTarjeta">
+          <div class="card tarjeta" style="width: 18rem;">
+            <img src="https://fakeimg.pl/300x150" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">${pelicula.title}</h5>
+              <p class="card-text">Director: ${pelicula.director}</p>
+              <p class="card-text">Año: ${pelicula.year}</p>
+              <p class="card-text">Rating: ${pelicula.rating}</p>
+            </div>
+          </div>
+        </div>
+      `;
+      listaCartelera.innerHTML += peliculaHTML;
+    });
+  } catch (error) {
+    console.error('Error al cargar películas:', error.message);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  cargarPeliculasCartelera1();
+  cargarPeliculasCartelera2();
+});
   
